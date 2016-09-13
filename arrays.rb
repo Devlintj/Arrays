f2866c32d5791c01bed1873d35a9527c71f8a006
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-13 13:35:25 -0600

    array complete

--- arrays.rb
+++ arrays.rb
@@ -1,28 +1,19 @@
-names = ['billy', 'suzie', 'greg', 'harambe']
-
-a = [1, 2, 3, 4, 5]
-
-names.each do |name|
-  print name + " "
-end
-
-#does a 4 exist in the list
-
-def hasFour?(a)
-  a.each do |n|
-    if n == 4
-      return true
+def haveThree(int)
+  three = 0
+  int.each do |i|
+    if i == 3
+      three += 1
+    end
   end
+
+    if three == 3
+      return true
+    end
 
   return false
+
 end
 
-# count how many 4s are in a list
-def count_four(a)
-  fours = 0
-  a.each do |n|
-    if n == 4
-      fours += 1
-    end
-    return fours
-  end
+puts haveThree([3, 1, 3, 1, 3])
+puts haveThree([3, 1, 3, 3, 3])
+puts haveThree([3, 3, 2, 1, 1])
