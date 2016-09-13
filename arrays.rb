fe94afc00b90217f9a283b0e13e4cfe7d3f5a385
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-13 13:45:40 -0600

    lucky13 complete

--- arrays.rb
+++ arrays.rb
@@ -6,9 +6,9 @@ def haveThree(int)
     end
   end
 
-    if three == 3
+  if three == 3
       return true
-    end
+  end
 
   return false
 
@@ -17,3 +17,23 @@ end
 puts haveThree([3, 1, 3, 1, 3])
 puts haveThree([3, 1, 3, 3, 3])
 puts haveThree([3, 3, 2, 1, 1])
+
+def lucky13?(int)
+  onethree = 0
+  int.each do |i|
+    if i == 3 || i == 1
+      onethree += 1
+    end
+  end
+
+  if onethree > 0
+    return false
+  end
+
+  return true
+end
+
+puts lucky13?([5, 2, 6, 8 , 9])
+puts lucky13?([3, 1, 1, 3])
+puts lucky13?([2, 8, 9, 3, 4])
+puts lucky13?([5, 6, 7, 1, 2])
