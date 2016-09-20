3214796ee560078964083754a4d91204ac1cffe6
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-20 13:42:13 -0600

    maxTriple complete

--- arrays1.rb
+++ arrays1.rb
@@ -11,3 +11,21 @@ puts reverse3([7, 6, 10])
 puts reverse3([6, 1, 10])
 puts reverse3([5, 2, 10])
 puts reverse3([4, 2, 13])
+
+def maxTriple(int)
+  num = 0
+  int.each do |i|
+    int.each do |n|
+      if n > i
+        i = n
+      end
+    end
+   num = i
+  end
+
+  print num
+end
+
+puts maxTriple([1,2,3])
+puts maxTriple([3,2,1])
+puts maxTriple([2,1,3])
