07f82fe93e22ba949d68f40c4c77c7b8b38f81d6
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-20 13:32:03 -0600

    reverse3 complete

---
+++ arrays1.rb
@@ -0,0 +1,13 @@
+def reverse3(int)
+  newarray = []
+  newarray.push(int[2])
+  newarray.push(int[1])
+  newarray.push(int[0])
+  print newarray
+end
+
+puts reverse3([8, 9, 10])
+puts reverse3([7, 6, 10])
+puts reverse3([6, 1, 10])
+puts reverse3([5, 2, 10])
+puts reverse3([4, 2, 13])
