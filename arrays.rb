764350fa5e9f358a9478ca2601e2d3e2ad7d8b9b
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-20 13:06:23 -0600

    post4 completed

--- arrays.rb
+++ arrays.rb
@@ -33,7 +33,38 @@ def lucky13?(int)
   return true
 end
 
+a = []
+a.push(1)
+a == [1] #true
+
 puts lucky13?([5, 2, 6, 8 , 9])
 puts lucky13?([3, 1, 1, 3])
 puts lucky13?([2, 8, 9, 3, 4])
 puts lucky13?([5, 6, 7, 1, 2])
+
+def post4(int)
+  placeholder = 0
+  newarray = []
+  int.each do |i|
+    if i == 4
+      placeholder += 1
+    end
+  end
+
+  int.each do |i|
+    if placeholder == 0
+      newarray.push(i)
+    elsif i == 4
+      placeholder -= 1
+    end
+  end
+
+  puts newarray
+end
+
+puts post4([4, 3, 2, 1])
+puts post4([3, 4, 2, 1])
+puts post4([3, 2, 4, 1])
+puts post4([4, 3, 4, 1])
+puts post4([4, 4, 4, 1])
+puts post4([3, 2, 1, 4])
