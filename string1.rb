0f2be1e2488f311fb23adc1865b9c9653f138135
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-22 14:23:58 -0600

    conCat completed

--- string1.rb
+++ string1.rb
@@ -14,3 +14,17 @@ puts comboString("wo", "hurt")
 puts comboString("words", "")
 puts comboString("wording", "  ")
 puts comboString("       ", "hurt")
+
+def conCat(str1, str2)
+  x = str2[0]
+  if str1.end_with?(x)
+    return str1.chop + str2
+  else
+    return str1 + str2
+  end
+end
+
+puts conCat("time", "each")
+puts conCat("tim", "each")
+puts conCat("dime", "peach")
+puts conCat("crime", "eat")
